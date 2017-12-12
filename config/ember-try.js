7df16379f1863a6668a7c8881a04cefc3734223e
Max Minkoff <max@planetminkoff.com>
2017-12-12 10:26:40 -0500

    Bump version to 0.0.8
    switch to testing with Chrome instead of PhantomJS

--- config/ember-try.js
+++ config/ember-try.js
@@ -34,6 +34,22 @@ module.exports = {
       }
     },
     {
+      name: 'ember-lts-2.12',
+      npm: {
+        devDependencies: {
+          'ember-source': '~2.12.0'
+        }
+      }
+    },
+    {
+      name: 'ember-lts-2.16',
+      npm: {
+        devDependencies: {
+          'ember-source': '~2.16.0'
+        }
+      }
+    },
+    {
       name: 'ember-release',
       bower: {
         dependencies: {
